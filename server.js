b2d88d65d0e679fff9f2e8ed496d006891a1947c
mayurwagh <mayurwagh1412@gmail.com>
2026-06-04 18:44:48 +0530

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(" Application is not done");
+    res.send(" Application is t done");
 });
 
 app.get("/health", (req, res) => {
