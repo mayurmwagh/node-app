f063505c660721e7bd4928a32b2785983446997e
mayurwagh <mayurwagh1412@gmail.com>
2026-07-10 16:21:01 +0530

    code change is performed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(" Application is now done");
+    res.send(" Application is now done with Latest build");
 });
 
 app.get("/health", (req, res) => {
