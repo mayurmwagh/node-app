95c0bd25f9d8b1760ca6bf1406f43a8ac742668f
mayurwagh <mayurwagh1412@gmail.com>
2026-07-15 14:31:54 +0530

    updated

--- k8s-deployment.groovy
+++ k8s-deployment.groovy
@@ -114,8 +114,8 @@ pipeline{
                 steps {
 
                     sh '''
-                    kubectl apply -f k8s/deployment.yaml
-                    kubectl apply -f k8s/service.yaml
+                    kubectl apply -f k8s/*
+                
                     '''
                 }
             }
